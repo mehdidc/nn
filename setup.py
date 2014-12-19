7f36841962cbbad93dd6aa5ba2bbb971394ed093
Mehdi Cherti <mehdi@cherti.name>
2014-12-19 13:49:23 +0100

    add setup.py and make it as library (the other commit did not work)

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='NNet',
+      version='0.1',
+      description='easy to use nn library',
+      author='Mehdi cherti',
+      author_email='mehdi@cherti.name',
+      url='notyet',
+      packages=['NNet'],
+     )
